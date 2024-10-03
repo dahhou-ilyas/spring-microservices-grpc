95d116cee3fdaaa1898968bf28385828c35dc476
dahhou.ilyas <dahhou.ilyas@gmail.com>
2024-10-03 23:46:52 +0100

    add city service

---
+++ city-score/src/main/java/com/example/cityscore/CityScoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.cityscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityScoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CityScoreApplication.class, args);
+    }
+
+}
---
+++ city-score/src/test/java/com/example/cityscore/CityScoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cityscore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityScoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
