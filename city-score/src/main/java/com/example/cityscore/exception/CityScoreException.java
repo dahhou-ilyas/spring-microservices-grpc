c43452b95922128b91ea300e007df0e16385ba46
dahhou.ilyas <dahhou.ilyas@gmail.com>
2024-10-03 23:53:05 +0100

    add exception for city score service

---
+++ city-score/src/main/java/com/example/cityscore/exception/CityScoreException.java
@@ -0,0 +1,15 @@
+package com.example.cityscore.exception;
+
+import com.nils.gprc.cityscore.CityScoreErrorCode;
+
+public class CityScoreException extends RuntimeException {
+
+    private static final long serialVersionUID = -8111656859346000121L;
+
+    private CityScoreErrorCode errorCode;
+
+    public CityScoreException(CityScoreErrorCode errorCode) {
+        super(errorCode.name());
+        this.errorCode = errorCode;
+    }
+}
