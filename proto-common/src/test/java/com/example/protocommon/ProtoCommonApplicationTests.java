565c0d608ff80104fb100cc1c2d0c624b92fd8fb
dahhou.ilyas <dahhou.ilyas@gmail.com>
2024-10-03 20:41:12 +0100

    create proto project

---
+++ proto-common/src/test/java/com/example/protocommon/ProtoCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.protocommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtoCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
